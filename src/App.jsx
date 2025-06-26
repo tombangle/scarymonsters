17599763d8b36245e083c60c96cf09c6bc78ef4b
Tom Bangle <tombangle@gmail.com>
2025-06-25 21:02:21 -0700

    Last Push

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
 
   return (
     <div className='App'>
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>Scary Monsters Contact Cards</h1>
 
       <SearchBox
         className='monsters-search-box'
